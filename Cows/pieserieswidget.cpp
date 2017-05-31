ea355d11b1aa322e7c7ee81ffddb5252e64270d9
liutimo <779564531@qq.com>
2017-05-31 13:03:47 +0800

    饼状图  数据显示

--- Cows/pieserieswidget.cpp
+++ Cows/pieserieswidget.cpp
@@ -10,19 +10,24 @@ PieSeriesWidget::PieSeriesWidget(QWidget *parent) : QDialog(parent)
 
     slice0 = series->slices().at(0);
     slice0->setBrush(Qt::red);
+    slice0->setLabelVisible();
 
     slice1 = series->slices().at(1);
     slice1->setBrush(Qt::green);
+    slice1->setLabelVisible();
 
     slice2 = series->slices().at(2);
     slice2->setBrush(Qt::yellow);
+    slice2->setLabelVisible();
 
     chart = new QChart();
     chart->addSeries(series);
     chart->setTitle("运动状态");
+    chart->legend()->hide();
 
     chartView = new QChartView(chart);
     chartView->setRenderHint(QPainter::Antialiasing);
+
 
     QHBoxLayout *layout = new QHBoxLayout;
     layout->addWidget(chartView);
@@ -31,9 +36,22 @@ PieSeriesWidget::PieSeriesWidget(QWidget *parent) : QDialog(parent)
 
 void PieSeriesWidget::setValue(int value1, int value2, int value3)
 {
+
+    qreal sum = (value1 + value2 + value3) * 1.0;
+    qreal v1, v2, v3;
+
+    v1 = value1 / sum * 100;
+    v2 = value2 / sum * 100;
+    v3 = 100 - v1 -v2;
+
     slice0->setValue(value1);
+    slice0->setLabel("运动" + QString::number(v1) + "%");
+
     slice1->setValue(value2);
+    slice1->setLabel("行走" + QString::number(v2) + "%");
+
     slice2->setValue(value3);
+    slice2->setLabel("未知" + QString::number(v3) + "%");
 }
 
 void PieSeriesWidget::setValue(const QList<int> &value)
